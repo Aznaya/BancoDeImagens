230240bb58bf73f6426fcb2233301707961f6f13
Jonas <53447664+Lebedeu@users.noreply.github.com>
2020-11-14 20:31:03 -0300

    Exibindo a lista de banco

--- banco-imagem-app/src/components/menuBanco/menuBanco.js
+++ banco-imagem-app/src/components/menuBanco/menuBanco.js
@@ -1,11 +1,47 @@
 import './menuBanco.css';
 
-function Banco() {
-  return (
-    <div className="Banco">
-      teste
+function ListarBanco(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <div key={number.toString()} onClick={(e) => click(number.toString(), e)} className="Botao">
+        {number}
     </div>
   );
+
+  function click(id, e) {
+    e.preventDefault();
+    console.log('Click...', id);
+  }
+
+  return (
+    <div className="Tabela">
+      {listItems}
+      <CriarBanco />
+    </div>
+  )
+}
+
+function CriarBanco() {
+  
+  return (
+    <div className="Botao_Adicionar">
+      +
+    </div>
+  )
+}
+
+const numbers = [1, 2, 3, 4, 5];
+const text = ['texto 1', 'texto 2', 'texto 3'];
+
+function Banco() {
+
+  return (
+    <div className="Banco">
+      <ListarBanco className="Banco" numbers={numbers} />
+      
+    </div>
+  );
+
 }
 
 export default Banco;
